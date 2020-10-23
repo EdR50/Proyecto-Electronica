2a9fc2143c6b8b7af0b1a26aef54d9785225e4cf
edR50 <narutogr7@gmail.com>
2020-10-23 08:06:48 -0300

    Update: SE ha terminado el proyecto.

--- js/prueba.js
+++ js/prueba.js
@@ -1,170 +1,159 @@
-// VARIABLES DE INTRODUCCION
-var vrms =0; 
-var fre =0; 
-var caida =0; 
-var vr =0; 
-var n1 =0; 
-var n2 =0;
-var cal=document.getElementById("calcular");
+let vrms = 0;
+let fre = 0;
+let caida = 0;
+let vr = 0;
+let n1 = 0;
+let n2 = 0;
+const cal = document.getElementById("calcular");
 
-// VARIABLES DE PRIMERA APROXIMACION
-var v1 = document.getElementById("v1").value;
-var v2 = document.getElementById("v2").value;
-var vp1 = document.getElementById("vp1").value;
-var vpp1 = document.getElementById("vpp1").value;
-var vp2i = document.getElementById("vp2i").value;
-var vp2o = document.getElementById("vp2o").value;
-var vpp2i = document.getElementById("vpp2i").value;
-var vpp2o = document.getElementById("vpp2o").value;
-var vdc = document.getElementById("vdc").value;
-// VARIABLES DE SEGUNDA APROXIMACION
-var vp2ii = document.getElementById("vp2ii").value;
-var vp2oo = document.getElementById("vp2oo").value;
-var vpp2ii = document.getElementById("vpp22i").value;
-var vpp2oo = document.getElementById("vpp22o").value;
-var vl = document.getElementById("vl2").value;
-var vdc2 = document.getElementById("vdc2").value;
+// PRIMERA APROXIMACIÓN
+let v1Data = document.getElementById("v1").value;
+let v2 = document.getElementById("v2").value;
+let vp1 = 0
+let vpp1 = document.getElementById("vpp1").value;
+let vp2i = 0;
+let vp2o = document.getElementById("vp2o").value;
+let vpp2i = document.getElementById("vpp2i").value;
+let vpp2o = document.getElementById("vpp2o").value;
+let vdc = document.getElementById("vdc").value;
 
-// PRIMERA APROXIMACION
-//    document.getElementById("vrms").value;
-//   document.getElementById("fre").value;
-//     document.getElementById("caida").value;
-//     document.getElementById("vr").value;
-//     document.getElementById("n1").value;
-//     document.getElementById("n2").value;
-function voltajep() {
-    vrms=document.getElementById("vrms").value;
-    var displayvp1=document.getElementById("vp1");
-    vp1 =parseFloat(vrms / 0.707);
-    displayvp1.textContent=vp1;
 
+// SEGUNDA APROXIMACIÓN
+let vp2ii = document.getElementById("vp2ii").value;
+let vp2oo = 0;
+let vpp2ii = document.getElementById("vpp22i").value;
+let vpp2oo = document.getElementById("vpp22o").value;
+let vl = document.getElementById("vl2").value;
+let vdc2 = document.getElementById("vdc2").value;
+
+// Working dope
+const voltajePHandler = () => {
+    vrms = document.getElementById("vrms").value;
+    let displayVp1 = document.getElementById("vp1");
+    vp1 = parseFloat(vrms)/0.707;
+    displayVp1.textContent = vp1.toFixed(2);
     return vp1;
 }
 
-function voltajepp() {
-    var displayvpp1=document.getElementById("vpp1");
-    vpp1 = parseFloat(2 * voltajep());
-    displayvpp1.textContent=vpp1;
+//Working well
+const voltajeppHandler = () => {
+    let displayvpp1 = document.getElementById("vpp1");
+    vpp1 = 2 * voltajePHandler();
+    displayvpp1.textContent = vpp1.toFixed(2);
     return vpp1;
 }
 
-function voltaje2() {
-    vrms=document.getElementById("vrms").value;
-    n1=document.getElementById("n1").value;
-    n2=document.getElementById("n2").value;
-    var div = parseFloat(n1 / n2);
-    var display=document.getElementById("v2");
-    v2 = parseFloat(vrms / div);
-    display.textContent=v2;
-
+// Working well
+const voltaje2Handler = () => {
+    vrms = document.getElementById("vrms").value;
+    n1 = document.getElementById("n1").value;
+    n2 = document.getElementById("n2").value;
+    let div = n1 / n2;
+    let display = document.getElementById("v2");
+    let display2 = document.getElementById("v22");
+    v2 = vrms / div;
+    display.textContent = v2.toFixed(2);
+    display2.textContent = v2.toFixed(2);
     return v2;
 }
 
-function v1(){
-    var displayv1=document.getElementById("v1");
-    v1=vrms;
-    displayv1.textContent=v1;
-    return v1;
+// working well
+const v1Handler = () => {
+    let displayV1 = document.getElementById("v1");
+    let displayV11 = document.getElementById("v11");
+    vrms = document.getElementById("vrms").value;
+    displayV1.textContent = vrms;
+    displayV11.textContent = vrms;
+    return vrms;
 }
 
-function vp2() {
-    var displayvp2i=document.getElementById("vp2i");
-    // var displayvpp2o=document.getElementById("vp2o");
-    vp2i = parseFloat(voltaje2() / 0.707);
-    // vp2o = parseFloat(voltaje2() / 0.707);
-    displayvp2i.textContent=vp2i;
-    // displayvpp2o.textContent=vp2o;
+// working well
+const vp2Handler = () => {
+    let displayvp2i = document.getElementById("vp2i");
+    let displayvp2ii = document.getElementById("vp2ii");
+    vp2i = voltaje2Handler() / 0.707;
+    displayvp2i.textContent = vp2i.toFixed(2);
+    displayvp2ii.textContent = vp2i.toFixed(2);
     return vp2i;
 }
 
-function vp2out() {
-    // var displayvpp2i=document.getElementById("vp2i");
-    var displayvp2o=document.getElementById("vp2o");
-    // vp2i = parseFloat(voltaje2() / 0.707);
-    vp2o = parseFloat(voltaje2() / 0.707);
-    // displayvpp2i.textContent=vp2i;
-    displayvp2o.textContent=vp2o;
+const vp2outHandler = () => {
+    let displayvp2o = document.getElementById("vp2o");
+    vp2o =voltaje2Handler() / 0.707
+    displayvp2o.textContent = vp2o.toFixed(2);
     return vp2o;
 }
 
-function vpp2() {
-    var displayvpp2i=document.getElementById("vpp2i");
-    var displayvpp2o=document.getElementById("vpp2o");
-    vpp2i =parseInt(vp2() * 2);
-    vpp2o =parseInt(vp2() * 2);
-    
-    displayvpp2i.textContent=vpp2i;
-    displayvpp2o.textContent=vpp2o;
+const vpp2Handler = () => {
+    let displayvpp2i = document.getElementById("vpp2i");
+    let displayvpp2o = document.getElementById("vpp2o");
+    vpp2i = vp2Handler() * 2;
+    vpp2o = vp2Handler() * 2;
 
-    return vpp2i, vpp2o;
+    displayvpp2i.textContent = vpp2i.toFixed(2);
+    displayvpp2o.textContent = vpp2o.toFixed(2);
+
+    return vpp2i;
 }
 
-function vdc() {
-    var displayvdc=document.getElementById("vdc");
-    vdc =(vp2out())/Math.PI;
-    displayvdc.textContent=vdc;
+const vdcHandler = () => {
+    let displayVdc = document.getElementById("vdc");
+    vdc = (vp2outHandler()) / Math.PI;
+    displayVdc.textContent = vdc.toFixed(2);
     return vdc;
 }
 
 
-// SEGUNDA APROXIMACION
+// SEGUNDA APROXIMACIÓN
 
-function vp2o() {
-    caida=document.getElementById("caida").value;
-    var displayvp2oo=document.getElementById("vp2oo");
-    vp2oo = parseInt(vp2() - caida);
-    displayvp2oo.textContent=vp2oo;
-
+const vp2oHandler = () => {
+    let displayVp2oo = document.getElementById("vp2oo");
+    caida = document.getElementById("caida").value;
+    vp2oo = vp2Handler() - caida
+    displayVp2oo.textContent = vp2oo.toFixed(2);
     return vp2oo;
-
 }
 
-function vpp2i() {
-    var vp2i = 35.36;
-    var vpp2i = document.write(parseInt(vp2i * 2));
+const vpp2iHandler = () => {
+    let displayVpp2i = document.getElementById("vpp22i");
+    let vpp2i = vp2Handler() * 2;
+    displayVpp2i.textContent = vpp2i.toFixed(2);
     return vpp2i;
 }
 
- function vpp2o() {
-     var vp2o = 34.66;
-     var vpp2o = parseInt(vp2o * 2);
-     return vpp2o;
- }
-
-
- function vdc2() {
-     var vp2o = 34.66;
-     var vdc2 = parseFloat(vp2o / Math.PI);
-     return vdc2;
- }
-
-cal.addEventListener('click',voltaje2);
-cal.addEventListener('click',voltajep);
-cal.addEventListener('click',voltajepp);
-cal.addEventListener('click',vp2);
-cal.addEventListener('click',vp2out);
-cal.addEventListener('click',vpp2);
-cal.addEventListener('click',vdc);
-cal.addEventListener('click',vp2o);
-
-// SOLO NUMEROS
-
-function solonumeros(e) {
-key = e.keyCode || e.which;
-teclado = String.fromCharCode(key);
-numeros = "0123456789";
-
-especiales = "8-37-38-46";
-
-teclado_especial = false;
-
-for (var i in especiales) {
-    if (key == especiales[i]) {
-        teclado_especial = true;
-    }
+const vpp2oHandler = () => {
+    let displayVpp2 = document.getElementById("vpp22o");
+    let vpp2o = vp2oHandler() * 2;
+    displayVpp2.textContent = vpp2o.toFixed(2);
+    return vpp2o;
 }
 
-if (numeros.indexOf(teclado) == -1 & !teclado_especial) {
-    return false;
+
+const vdc2Handler = () => {
+    let displayVdc = document.getElementById("vdc2");
+    let vdc2 = vp2outHandler() / Math.PI;
+    displayVdc.textContent = vdc2.toFixed(2)
+    return vdc2;
 }
+
+const vlHandler = () => {
+    let formula = vp2outHandler() / Math.PI;
+    let display = document.getElementById("vl2");
+    display.textContent = formula;
+
+    return formula;
 }
+
+cal.addEventListener('click', voltaje2Handler);
+cal.addEventListener('click', voltajePHandler);
+cal.addEventListener('click', voltajeppHandler);
+cal.addEventListener('click', vp2Handler);
+cal.addEventListener('click', vp2outHandler);
+cal.addEventListener('click', vpp2Handler);
+cal.addEventListener('click', vdcHandler);
+cal.addEventListener('click', vp2oHandler);
+cal.addEventListener('click', v1Handler);
+cal.addEventListener('click', vpp2iHandler);
+cal.addEventListener('click' , vpp2oHandler);
+cal.addEventListener('click' , vdc2Handler);
+cal.addEventListener('click' , vlHandler);
